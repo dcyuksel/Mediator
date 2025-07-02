e36b268f85e8e7a44548b100b200e37e8e579918
Yuksel, D.C. (Deniz) <deniz.yuksel@ing.com>
2025-07-02 15:43:30 +0200

    service provider is used

--- Mediator/Mediator/ISender.cs
+++ Mediator/Mediator/ISender.cs
@@ -3,6 +3,6 @@
 namespace Mediator;
 public interface ISender
 {
-    Task SendAsync(IRequest request, CancellationToken cancellationToken = default);
+    Task SendAsync<TRequest>(TRequest request, CancellationToken cancellationToken = default) where TRequest : IRequest;
     Task<TResponse> SendAsync<TResponse>(IRequest<TResponse> request, CancellationToken cancellationToken = default);
 }
--- Mediator/Mediator/Mediator.cs
+++ Mediator/Mediator/Mediator.cs
@@ -9,24 +9,15 @@ public sealed class Mediator(IServiceProvider serviceProvider) : IMediator
 {
     private static readonly ConcurrentDictionary<Type, Type> HandlerTypes = new();
 
-    public Task SendAsync(IRequest request, CancellationToken cancellationToken = default)
+    public async Task SendAsync<TRequest>(TRequest request, CancellationToken cancellationToken = default) where TRequest : IRequest
     {
         cancellationToken.ThrowIfCancellationRequested();
         ArgumentNullException.ThrowIfNull(request);
 
-        var requestType = request.GetType();
-        if (!HandlerTypes.TryGetValue(requestType, out var handlerType))
-        {
-            handlerType = typeof(IRequestHandler<>).MakeGenericType(requestType);
-            HandlerTypes.TryAdd(requestType, handlerType);
-        }
+        var handler = serviceProvider.GetService<IRequestHandler<TRequest>>();
+        HandlerNotFoundException.ThrowIfHandlerNull(handler, request.GetType().Name);
 
-        var handler = serviceProvider.GetService(handlerType);
-        HandlerNotFoundException.ThrowIfHandlerNull(handler, requestType.Name);
-
-        return (Task)handlerType
-                    .GetMethod("HandleAsync")?
-                    .Invoke(handler, [request, cancellationToken])!;
+        await handler!.HandleAsync(request, cancellationToken);
     }
 
     public async Task<TResponse> SendAsync<TResponse>(IRequest<TResponse> request, CancellationToken cancellationToken = default)
@@ -52,6 +43,7 @@ public sealed class Mediator(IServiceProvider serviceProvider) : IMediator
     public async Task PublishAsync<TNotification>(TNotification notification, CancellationToken cancellationToken = default) where TNotification : INotification
     {
         cancellationToken.ThrowIfCancellationRequested();
+        ArgumentNullException.ThrowIfNull(notification);
 
         var handlers = serviceProvider.GetServices<INotificationHandler<TNotification>>().ToList();
         if (handlers.Count == 0)
